36dc5e92ecd232f5c026632de780beb7cf83c347
2202008r <2202008r@student.gla.ac.uk>
2017-01-27 17:38:17 +0000

    Chapter 5 exercise, tryig to fix admin and error

--- Rango/views.py
+++ Rango/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from Rango.models import Category
 
 def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
-    context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
+    category_list = Category.objects.order_by('-likes')[:5]
+    context_dict = {'categories': category_list}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
--- populate_Rango.py
+++ populate_Rango.py
@@ -33,9 +33,9 @@ def populate():
          {"title":"Flask",
           "url":"http://flask.pocoo.org"} ]
 
-    cats = {"Python": {"pages": python_pages},
-             "Django": {"pages": django_pages},
-             "Other Frameworks": {"pages": other_pages} }
+    cats = {"Python": {"pages": python_pages, "likes": 64, "views": 168},
+             "Django": {"pages": django_pages, "likes": 32, "views": 64 },
+             "Other Frameworks": {"pages": other_pages, "likes": 16, "views": 32} }
 
      # If you want to add more catergories or pages,
      # add them to the dictionaries above.
@@ -47,8 +47,8 @@ def populate():
      # for more information about how to iterate over a dictionary properly.
 
     for cat, cat_data in cats.items():
-         c = add_cat(cat)
-         for p in cat_data["pages"]:
+        c = add_cat(cat, cat_data["likes"], cat_data["views"])
+        for p in cat_data["pages"]:
              add_page(c, p["title"], p["url"])
 
      # Print out the categories we have added.
@@ -63,8 +63,10 @@ def add_page(cat, title, url, views=0):
      p.save()
      return p
 
-def add_cat(name):
+def add_cat(name,likes,views):
      c = Category.objects.get_or_create(name=name)[0]
+     c.likes=likes;
+     c.views=views;
      c.save()
      return c
 
