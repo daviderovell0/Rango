8fc4195cde25507179fd350951874c99757981f5
Davide Rovelli <daviderovelli24gmail.com>
2017-01-30 22:41:59 +0000

    Chapter 7 - addCategory form added + clean() method in addPage

--- Rango/urls.py
+++ Rango/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^category/(?P<categoryNameSlug>[\w\-]+)/$',views.show_category, name='show_category'),
     url(r'^about/', views.about, name='about'),
+    url(r'^add_category/', views.add_category, name='add category')
 ]
--- Rango/views.py
+++ Rango/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from Rango.models import Category
 from Rango.models import Page
+from Rango.forms import CategoryForm
 
 def about(request):
     return render(request,"Rango/about.html")
@@ -28,3 +29,14 @@ def index(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'Rango/index.html', context=context_dict)
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    return render(request,'Rango/add_category.html', {'form':form})
