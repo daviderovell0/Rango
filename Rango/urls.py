513f60594f078ff36142b02e3fa33fc0e0d870e0
2202008r <2202008r@student.gla.ac.uk>
2017-02-06 19:18:04 +0000

    Chapter 9.6

--- Rango/urls.py
+++ Rango/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^category/(?P<categoryNameSlug>[\w\-]+)/$',views.show_category, name='show_category'),
     url(r'^about/', views.about, name='about'),
     url(r'^add_category/', views.add_category, name='addCategory'),
-    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',views.add_page, name="add_page")
-]
+    url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',views.add_page, name="add_page"),
+    url(r'^register/$',views.register,name='register'),
+    ]
